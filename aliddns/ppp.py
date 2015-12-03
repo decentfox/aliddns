bcf37c033aa371f7c887ca06479d3b0d0adb1845
Fantix King <fantix.king@gmail.com>
2015-12-03 13:51:10 +0800

    Initial 0.1 commit, ppp only

---
+++ aliddns/ppp.py
@@ -0,0 +1,68 @@
+import logging
+import sys
+
+INTERFACES = ['ppp0']
+TYPE = u'A'
+DOMAIN_NAME = u'example.com'
+RRS = [u'@', u'www']
+ALIYUN_KEY_ID = ''
+ALIYUN_KEY_SECRET = ''
+
+try:
+    from aliddns_config import *
+except ImportError:
+    pass
+
+
+def main():
+    import aliyun.api
+
+    interface = sys.argv[1]
+    if interface not in INTERFACES:
+        return
+
+    ip = sys.argv[4].decode('utf-8')
+
+    logging.info('Detected %s IP change: %s', interface, ip)
+
+    aliyun.setDefaultAppInfo(ALIYUN_KEY_ID, ALIYUN_KEY_SECRET)
+
+    req = aliyun.api.dns.DnsDescribeDomainRecordsRequest()
+    req.DomainName = DOMAIN_NAME
+    req.TypeKeyWord = TYPE
+
+    for rr in RRS:
+        req.RRKeyWord = rr
+        records = req.getResponse()['DomainRecords']['Record']
+        if records:
+            record = records[0]
+            if record['Value'] == ip:
+                logging.debug('%s.%s is already %s', rr, DOMAIN_NAME, ip)
+            else:
+                logging.info('Updating %s.%s from %s to %s', rr, DOMAIN_NAME,
+                             record['Value'], ip)
+                update = aliyun.api.dns.DnsUpdateDomainRecordRequest()
+                update.RecordId = record['RecordId']
+                update.RR = rr
+                update.Type = TYPE
+                update.Value = ip
+                update.getResponse()
+        else:
+            logging.warning('No such record %s.%s, skipping for now.', rr,
+                            DOMAIN_NAME)
+
+    logging.info('Finished DDNS process.')
+
+
+def main_wrapper():
+    logging.basicConfig(filename='/var/log/aliyun-ddns.log',
+                        level=logging.INFO,
+                        format='%(asctime)s %(levelname)s: %(message)s')
+    try:
+        main()
+    except Exception:
+        logging.exception('Failed due to exception!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,73 @@
+from codecs import open  # To use a consistent encoding
+from os import path
+
+from setuptools import setup, find_packages
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='aliddns',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.1',
+
+    description='DDNS tool for domains hosted on AliYun',
+    long_description=long_description,
+
+    url='http://git.decentfox.com/server',
+
+    author='DecentFoX Studio',
+    author_email='support@decentfox.com',
+
+    license='BSD',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='aliyun ddns',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    # namespace_packages=[],
+
+    install_requires=[
+        'aliyun-python-sdk',
+    ],
+
+    include_package_data=True,
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        # 'sample': ['package_data.dat'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[
+    ],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'aliyun-ddns-ppp=aliddns.ppp:main_wrapper',
+        ],
+    },
+)
